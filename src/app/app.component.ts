ae9306b780cf3c21344b29e746b99b4e8349ed11
teja <msaiteja.kuncham@gmail.com>
2022-10-11 21:06:30 +0530

    save work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'newer';
+hi:any[]=[];
+
+  constructor(private _service:DataService){
+  }
+
+ngOnInit(){
+  this.hello();
+}
+
+  hello(){
+    this._service.gettingApi().subscribe(
+      (response:any)=>{
+        this.hi=response;
+        console.log(response);
+      },
+      (error:any)=>console.log(error)
+    )
+  }
+
+
 }
